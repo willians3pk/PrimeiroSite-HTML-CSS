cfdd8b0d8e73ccc6f7f600f06cfcbfdf46079b9a
Willian silva de almeida <willianpk2010@outlook.com>
2020-07-30 21:47:06 -0300

    commit

---
+++ exercicio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Jeová Nissi - Home</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/index.css'>
+    <script src='php/main.php' type="text/php"></script>
+</head>
+
+<body>
+
+    <header class="cabecalho">
+       <h1>Curso PHP</h1>
+       <h2>Visualização do Exercício</h2>
+    </header>
+
+    <main class="principal">
+        <div class="conteudo">
+                
+        </div>
+    </main>
+    <footer class="rodape">
+        CONGREGAÇÃO JEOVÁ NISSI © <?= date('Y'); ?>
+    </footer>
+
+</body>
+
+</html>
